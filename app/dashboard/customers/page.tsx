b94ee79958b563744bb5ed17090c79354f32a462
olvrbntn <oliverbentonjavier@gmail.com>
2023-11-29 23:16:00 -0800

    finished all lessons

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from 'next';
+import CustomersTable from '@/app/ui/customers/table';
+ 
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
 export default function Page() {
     return <p>Customers Page</p>;
   }
--- app/dashboard/invoices/error.tsx
+++ app/dashboard/invoices/error.tsx
@@ -1,6 +1,8 @@
 'use client';
  
 import { useEffect } from 'react';
+
+
  
 export default function Error({
   error,
